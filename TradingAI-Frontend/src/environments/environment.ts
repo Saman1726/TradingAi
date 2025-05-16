49ebd3abbd691efca3fe518ef8c75055f206d6d0
Saman Anvari <saman1726@gmail.com>
2025-05-16 14:22:23 +0200

    fix: secrets

--- TradingAI-Frontend/src/environments/environment.ts
+++ TradingAI-Frontend/src/environments/environment.ts
@@ -3,7 +3,6 @@ export const environment = {
   apiUrl: 'http://localhost:8080/api', // Base URL for the API
   oauth2: {
     clientId: '154400877371-s68r5nv4vd1qllo5jqd0fm8ihqgfna17.apps.googleusercontent.com',
-    clientSecret: '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',
     redirectUri: 'http://localhost:8080/login/oauth2/code/google',
     scope: 'read write',
     tokenEndpoint: 'https://oauth2.googleapis.com/token',
