a9a6df98f7905feca3592d491941afdbe4c7a1f7
Saman Anvari <saman1726@gmail.com>
2025-05-16 14:25:37 +0200

    fix

--- TradingAI-Frontend/src/environments/environment.prod.ts
+++ TradingAI-Frontend/src/environments/environment.prod.ts
@@ -2,7 +2,6 @@ export const environment = {
   production: true,
   apiUrl: 'https://api.tradingai.com', // Production API URL
   oauth2: {
-    clientId: '154400877371-s68r5nv4vd1qllo5jqd0fm8ihqgfna17.apps.googleusercontent.com',
     redirectUri: 'http://localhost:8080/login/oauth2/code/google',
     scope: 'read write'
   }
--- TradingAI-Frontend/src/environments/environment.ts
+++ TradingAI-Frontend/src/environments/environment.ts
@@ -2,8 +2,6 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:8080/api', // Base URL for the API
   oauth2: {
-    clientId: '154400877371-s68r5nv4vd1qllo5jqd0fm8ihqgfna17.apps.googleusercontent.com',
-    redirectUri: 'http://localhost:8080/login/oauth2/code/google',
     scope: 'read write',
     tokenEndpoint: 'https://oauth2.googleapis.com/token',
     authorizationEndpoint: 'https://accounts.google.com/o/oauth2/auth',
