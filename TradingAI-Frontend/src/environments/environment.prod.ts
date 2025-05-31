37306026d55f9b601c6b58c8df60634f963159fc
Saman Anvari <saman1726@gmail.com>
2025-05-31 11:08:32 +0200

    fix: minor

--- TradingAI-Frontend/src/environments/environment.prod.ts
+++ TradingAI-Frontend/src/environments/environment.prod.ts
@@ -1,12 +1,10 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://register-gharaboroun.com/api', // Base URL for the API
   apiUrl: 'http://backend:8080/api', // Base URL for the API
   oauth2: {
     scope: 'read write',
     tokenEndpoint: 'https://oauth2.googleapis.com/token',
     authorizationEndpoint: 'https://accounts.google.com/o/oauth2/auth',
-    clientId: '154400877371-s68r5nv4vd1qllo5jqd0fm8ihqgfna17.apps.googleusercontent.com', 
-    clientSecret: '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', // Replace with your actual client secret
+    clientId: '154400877371-s68r5nv4vd1qllo5jqd0fm8ihqgfna17.apps.googleusercontent.com'
   }
 };
